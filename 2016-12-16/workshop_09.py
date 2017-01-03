f0e1d5c2711903b0cf54a2faa5311a1ed49f24af
giuliom95 <giuliomartella1995@gmail.com>
2017-01-03 17:00:02 +0100

    Work on workshop 9

--- 2016-12-16/workshop_09.py
+++ 2016-12-16/workshop_09.py
@@ -25,53 +25,6 @@ def plane_hpc(coeffs, dim=1):
     faces = [[i+1] for i in range(len(points))] 
     
     return JOIN([MKPOL([points, faces, [1]])])
-    
-    
-def det_steep(vic, vi1, vi2, vs):
-    xc, yc, _ = vs[vic-1]
-    x1, y1, _ = vs[vi1-1]
-    x2, y2, _ = vs[vi2-1]
-    dx1 = x1 - xc
-    dx2 = x2 - xc
-    dy1 = y1 - yc
-    dy2 = y2 - yc
-    
-    print 'Delta:',dx1, dy1, dx2, dy2
-    
-    #1
-    if dx2 > 0 and dy1 > 0:
-        print 1
-        return 1, 1
-    #2
-    if dx1 > 0 and dy2 > 0:
-        print 2
-        return -1, -1
-    #3
-    if dx2 < 0 and dy1 > 0:
-        print 3
-        return -1, 1
-    #4
-    if dx1 < 0 and dy2 > 0:
-        print 4
-        return -1, 1
-    #5
-    if dx1 < 0 and dy2 < 0:
-        print 5
-        return -1, -1
-    #6
-    if dx2 < 0 and dy1 < 0:
-        print 6
-        return 1, 1
-    #7
-    if dx2 > 0 and dy1 < 0:
-        print 7
-        return 1, 1
-    #8
-    if dx1 > 0 and dy2 < 0:
-        print 8
-        return -1, 1
-    
-    return None
 
 
 def plane_through_xyvector(vec, angle):
@@ -113,10 +66,13 @@ def calc_near_vs(vi, vertices, faces):
     A = [f[fix_pos(f.index(vi)+1, len(f))] for f in fs]
     B = [f[fix_pos(f.index(vi)-1, len(f))] for f in fs]
     tutti = A + B
-    return [v for v in tutti if not (v in A and v in B)]
+    return [v for v in tutti if not (v in A and v in B)][::-1]
 
 
 def build_roof_plane(vi1, vi2, vs, angle):
+    """
+    Builds a plane given two points and an angle
+    """
     plane = plane_through_xyvector(
         [vts[vi1-1][0]-vts[vi2-1][0], vts[vi1-1][1]-vts[vi2-1][1]],
         angle)
@@ -124,9 +80,45 @@ def build_roof_plane(vi1, vi2, vs, angle):
 
 
 def intersection(plane1, plane2, plane3):
+    """Gets the intersection between three planes"""
     A = np.array([plane1[:3], plane2[:3], plane3[:3]])
     B = -1*np.array([plane1[-1], plane2[-1], plane3[-1]])
     return list(np.linalg.solve(A,B))
+
+
+def get_vector(i1, i2, vertices):
+    """
+    Returns the vector between two points
+    """
+    p1, p2 = np.array([vertices[i1-1], vertices[i2-1]])
+    return p2 - p1
+    
+    
+def get_coeffs(v):
+    """
+    Black magic
+    """
+    return -np.sign(v[0]), np.sign(v[1])
+
+
+def ggpl_workshop_09(vts, fcs, roof_height, angle):
+    roof_top = [0,0,1,-roof_height]
+    new_vts = []
+    
+    for vic in range(1,len(vts)+1):
+        vi1, vi2 = calc_near_vs(vic, vts, fcs)
+        
+        v1 = get_vector(vi1, vic, vts)
+        v2 = get_vector(vic, vi2, vts)
+        c1, c2 = get_coeffs(v1 + v2)
+        print v1, v2, v1+v2, c1, c2
+        
+        plane1 = build_roof_plane(vic, vi1, vts, c1*angle)
+        plane2 = build_roof_plane(vic, vi2, vts, c2*angle)
+        
+        new_vts += [intersection(roof_top, plane1, plane2)]
+        
+    return MKPOL([new_vts, fcs, [1]])
 
 
 if __name__=='__main__':
@@ -145,37 +137,12 @@ if __name__=='__main__':
         [1,4,7],
         [4,5,6,7]]
     
-    roof_top = [0,0,1,-.5]
-    new_vts = []
-    to_view = []
-    for vic in range(1,len(vts)+1):
-    #for vic in [4]:
-        vi1, vi2 = calc_near_vs(vic, vts, fcs)
-        
-        print 'Punto:',vic
-        angle = np.pi/6
-        c1, c2 = det_steep(vic, vi1, vi2, vts)
-        #, c1, c2
-        
-        plane1 = build_roof_plane(vic, vi1, vts, c1*angle)
-        plane2 = build_roof_plane(vic, vi2, vts, c2*angle)
-        
-        #to_view += [plane_hpc(plane1, 4), plane_hpc(plane2, 4)] 
-        new_vts += [intersection(roof_top, plane1, plane2)]
-    
+    hpc = ggpl_workshop_09(vts, fcs, .8, np.pi/6)
     
     to_view = [
         SKEL_1(MKPOL([vts, fcs, [1]])), 
-        SKEL_1(MKPOL([new_vts, fcs, [1]]))]
+        SKEL_1(hpc)]
     
      
     VIEW(STRUCT(to_view))
-    """
-    toview = [
-        plane_hpc(plane1, 2),
-        plane_hpc(plane2, 2),
-        plane_hpc(roof_top, 2)]
-    
-    VIEW( STRUCT(toview) )
-    """
     
