2d869808a4bf76d9c0a6f110c311243dac69edfb
giuliom95 <giuliomartella1995@gmail.com>
2017-02-01 00:02:22 +0100

    Added variance to trees

--- 2017-01-27/trees.py
+++ 2017-01-27/trees.py
@@ -11,9 +11,10 @@ def tree():
     faces = [[i+1] for i in range(30)]
     
     trunk_height = np.random.rand()+1.5
+    leaves_scale = .3*np.random.rand(2)+1
     
     return T([1,2,3])([-.1,-.1,trunk_height])(STRUCT([
-        S([1,2])([1.2, 1.2])(JOIN([MKPOL([points, faces, [1]])])),
+        S([1,2])(leaves_scale)(JOIN([MKPOL([points, faces, [1]])])),
         TEXTURE(['wood.jpg',True, True, 300,300, 0, .1,.1, 0,0])(
             S(3)(-1)(CUBOID([.2,.2,trunk_height])))
         ]))
